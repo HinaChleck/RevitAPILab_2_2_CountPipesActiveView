feac1bf9b9cc2efbe7cb9d3dfff5ac0c6d18dc7a
HinaChleck <HinaChleck@gmail.com>
2022-04-02 14:08:48 +0500

    Добавьте файлы проекта.

---
+++ RevitAPILab_2_2_CountPipesActiveView/Main.cs
@@ -0,0 +1,31 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPILab_2_2_CountPipesActiveView
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<Pipe> pipes = new FilteredElementCollector(doc, doc.ActiveView.Id)
+            .OfClass(typeof(Pipe))
+            //.WhereElementIsNotElementType() - не требуется при фильтрации по классу (требуется при фильтрации по категории)
+            .Cast<Pipe>()
+            .ToList();
+            TaskDialog.Show("Количество труб на виде: ", pipes.Count.ToString());
+            return Result.Succeeded;
+        }
+    }
+}
